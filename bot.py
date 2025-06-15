59c59e432479ec99a37db60e3555b4771f95d420
Residential Kiwi <113327928+ResidentKiwi@users.noreply.github.com>
2025-06-14 23:22:37 -0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,14 @@
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, CallbackQueryHandler, filters
+import os
+from handlers import start, criar_grupo, adicionar_canal, aceitar_convite, new_post, handle_callback_query
+
+TOKEN = os.getenv("TELEGRAM_TOKEN")
+
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("criar_grupo", criar_grupo))
+app.add_handler(CommandHandler("adicionar_canal", adicionar_canal))
+app.add_handler(CallbackQueryHandler(handle_callback_query))
+app.add_handler(MessageHandler(filters.ChatType.CHANNELS & filters.ALL, new_post))
+
+app.run_polling()
