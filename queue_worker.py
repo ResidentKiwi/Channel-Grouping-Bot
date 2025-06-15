e47b1586913a06f0578c109402ea63a72b22f7d0
Residential Kiwi <113327928+ResidentKiwi@users.noreply.github.com>
2025-06-15 12:01:46 -0300

    Update queue_worker.py

--- queue_worker.py
+++ queue_worker.py
@@ -3,5 +3,5 @@ import os
 
 BOT = Bot(os.getenv("TELEGRAM_TOKEN"))
 
-def forward(src_chat_id, dst_chat_id, message_id):
-    BOT.forward_message(chat_id=dst_chat_id, from_chat_id=src_chat_id, message_id=message_id)
+async def forward(src_chat_id, dst_chat_id, message_id):
+    await BOT.forward_message(chat_id=dst_chat_id, from_chat_id=src_chat_id, message_id=message_id)
