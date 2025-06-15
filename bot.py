1ec9bbfc18e15f06f2af90741c08b5ff6ba00df8
Residential Kiwi <113327928+ResidentKiwi@users.noreply.github.com>
2025-06-15 09:38:00 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,6 +9,6 @@ app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("criar_grupo", criar_grupo))
 app.add_handler(CommandHandler("adicionar_canal", adicionar_canal))
 app.add_handler(CallbackQueryHandler(handle_callback_query))
-app.add_handler(MessageHandler(filters.ChatType.CHANNEL & filters.ALL, new_post))  # <- corrigido
+app.add_handler(MessageHandler(filters.ChatType.CHANNEL & filters.ALL, new_post))
 
 app.run_polling()
