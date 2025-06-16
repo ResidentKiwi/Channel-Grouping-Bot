9e4bb2c14cbcb84a7488eb095692a87ee77e9863
Residential Kiwi <113327928+ResidentKiwi@users.noreply.github.com>
2025-06-16 17:54:46 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,8 @@
 import os, logging
 from fastapi import FastAPI, Request
 from telegram import Update, Bot
-from telegram.ext import (
-    ApplicationBuilder,
-    CommandHandler,
-    CallbackQueryHandler,
-    MessageHandler,
-    filters,
-)
-from handlers import (
-    start,
-    channel_authenticate,
-    new_post,
-    handle_callback_query,
-    handle_text_message,
-)
+from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, MessageHandler, filters
+from handlers import start, channel_authenticate, new_post, handle_callback_query, handle_text_message
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -24,7 +12,6 @@ WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 PORT = int(os.getenv("PORT", "10000"))
 
 bot_app = ApplicationBuilder().token(TOKEN).build()
-
 bot_app.add_handler(CommandHandler("start", start))
 bot_app.add_handler(CallbackQueryHandler(handle_callback_query))
 bot_app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_text_message))
@@ -36,11 +23,11 @@ app = FastAPI()
 
 @app.on_event("startup")
 async def startup():
-    logger.info("🌐 Inicializando bot")
+    logger.info("Inicializando bot...")
     await bot_app.initialize()
     await telegram_bot.delete_webhook()
     await telegram_bot.set_webhook(WEBHOOK_URL)
-    logger.info("⭐ Webhook configurado com sucesso!")
+    logger.info("Webhook configurado com sucesso!")
 
 @app.post("/webhook")
 async def webhook(request: Request):
@@ -54,5 +41,5 @@ async def root():
 
 if __name__ == "__main__":
     import uvicorn
-    logger.info("🚀 Rodando localmente")
+    logger.info("Executando localmente")
     uvicorn.run("bot:app", host="0.0.0.0", port=PORT, reload=True)
