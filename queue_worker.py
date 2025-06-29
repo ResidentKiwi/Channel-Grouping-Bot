d70df90a87a40144a259d7a6880af8308b62e4d8
Residential Kiwi <113327928+ResidentKiwi@users.noreply.github.com>
2025-06-29 16:35:26 -0300

    Update queue_worker.py

--- queue_worker.py
+++ queue_worker.py
@@ -1,7 +1,9 @@
 from telegram import Bot
+from telegram.request import AiohttpSession
 import os
 
-BOT = Bot(os.getenv("TELEGRAM_TOKEN"))
+session = AiohttpSession()
+BOT = Bot(os.getenv("TELEGRAM_TOKEN"), request=session)
 
 async def forward(src_chat_id, dst_chat_id, message_id):
     await BOT.forward_message(chat_id=dst_chat_id, from_chat_id=src_chat_id, message_id=message_id)
